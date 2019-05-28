b9b32c955921936b6bbbf443e460b71d4693591c
Unknown <p.malinowski00@googlemail.com>
2019-05-28 23:27:43 +0200

    update

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -34,28 +34,44 @@ var data = [{
 
 
 
-function Box() {
+function Box(boxData) {
 
-    const [{
-        id: boxId,
-        title: boxTitle,
-        content: boxContent,
-    }] = data;
+    for (var i = 0; i < data.length; i++) {
+        var boxId = boxData[i].id;
+        var boxTitle = boxData[i].title;
+        var boxContent = boxData[i].content;
+        var categoriesIndexZero = boxData[i].categories[0];
+        var categoriesIndexOne = boxData[i].categories[1];
+        var categoriesIndexTwo = boxData[i].categories[2];
 
-    var newBox = document.createElement('div');
-    var newTitle = document.createElement('title');
-    var newHeader = document.createElement('header');
+        console.log(newBox)
 
-    newBox.className = 'box box-bg';
-    newHeader.className = 'header header-special';
-    newTitle.className = 'title';
+        var newBox = document.createElement('div');
+        var newHeader = document.createElement('header');
 
+        newBox.className = 'box';
+        newHeader.className = 'header';
+        console.log(newBox)
+        console.log(categoriesIndexZero, categoriesIndexOne, categoriesIndexTwo)
 
-    newTitle.innerHTML = boxTitle;
-    newHeader.innerHTML = newTitle + boxContent;
-    newBox.innerHTML = newHeader;
-    newBox.id = boxId;
-    document.body.appendChild(newBox).appendChild(newHeader).appendChild(newTitle);
+        newHeader.innerHTML = boxTitle + boxContent;
+        newBox.id = boxId;
+        document.body.appendChild(newBox).appendChild(newHeader);
+
+        if (categoriesIndexZero || categoriesIndexOne || categoriesIndexTwo == 'highlighted') {
+            document.getElementById(boxId).classList.add('highlighted');
+            console.log(newBox)
+        }
+        if (categoriesIndexZero || categoriesIndexOne || categoriesIndexTwo == 'special-header') {
+            console.log(newBox)
+            newHeader.classList.add('special-header');
+        }
+        if (categoriesIndexZero || categoriesIndexOne || categoriesIndexTwo == 'important') {
+            console.log(newBox)
+            document.getElementById(boxId).classList.add('important');
+        }
+
+    }
 
 }
-Box(data[2]);
+Box(data);
